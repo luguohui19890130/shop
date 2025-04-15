7c40ae8d8cd886603552b9e72cf6059a476ec92c
luguohui19890130 <vincent44544>
2025-04-15 10:43:04 +0800

    idea还原

--- src/main/java/xyz/yyds888/Test.java
+++ src/main/java/xyz/yyds888/Test.java
@@ -9,11 +9,7 @@ package xyz.yyds888;
 public class Test {
 
     public static void main(String[] args) {
-        User.say();
         System.out.println("你好！");
-        System.out.println("在github上编辑1次，在idea编辑1次");
-        System.out.println("在github上编辑3次");
-        System.out.println("完成idea与github合并1次");
     }
 
 }
