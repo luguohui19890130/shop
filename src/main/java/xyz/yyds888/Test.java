acf99ea728c04c887a0c26fc5e9ffa0fb567aa32
luguohui19890130 <vincent44544>
2025-04-15 09:38:34 +0800

    Changes

---
+++ src/main/java/xyz/yyds888/Test.java
@@ -0,0 +1,16 @@
+package xyz.yyds888;
+
+/**
+ * @class:
+ * @description:
+ * @author: lugh
+ * @createDate: 2022年02月24日 17:21
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        User.say();
+
+    }
+
+}
